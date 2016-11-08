993f2cecf32b581ec4b008a81e015fda44f4c1a3
KostiaSA <sonyk795>
2016-11-08 20:08:15 +0300

    --

--- App/pages/LoginPage.tsx
+++ App/pages/LoginPage.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {getDeepClone} from "../utils/getDeepClone";
-import {Input} from "../components/Input";
 
 
 export interface ILoginPageProps {
